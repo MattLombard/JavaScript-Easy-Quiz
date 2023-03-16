85256caa5f922087a1dd3acc876682f344faa941
Matt Lombard <ncmattlombard@gmail.com>
2023-03-15 20:35:29 -0400

    currently have most everything working except the go back button at this time. need to figure out what the issue is so that when i press save score i can still go back

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,27 +4,32 @@ const timerElement = document.getElementById("timer");
 const timeRemainingElement = document.getElementById("time-remaining");
 const quizContainer = document.getElementById("quiz-container");
 const questionElement = document.getElementById("question");
-const choicesContainer = document.getElementById("choices-all");
+const choicesContainer = document.getElementById("choices-container");
 const gameOver = document.getElementById("game-over");
 const scoreElement = document.getElementById("score");
 const initialsInput = document.getElementById("initials");
 const saveScoreButton = document.getElementById("save-score");
 const highScoresContainer = document.getElementById("high-scores");
-const goBackButton = document.getElementById("go-back"); 
-const splashScreenContainer = document.getElementById("splash-screen"); 
+const goBackButton = document.getElementById("go-back");
+const splashScreenContainer = document.getElementById("splash-screen");
 
 // Questions array
 const questions = [
-    {
-        text: "What does a .js file refer to?",
-        choices: ["JuicySeed", "JavaScript", "Juicy J", "JustShoot"], 
-        correctIndex: 1,
-    },
-    {
-        text: "Inside which HTML element do we put the JavaScript?",
-        choices: ["<script>", "<body>", "<footer>", "<scripting>"],
-        correctIndex: 0,
-    },
+  {
+    text: "What does a .js file refer to?",
+    choices: ["JuicySeed", "JavaScript", "Juicy J", "JustShoot"],
+    correctIndex: 1,
+  },
+  {
+    text: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<script>", "<body>", "<footer>", "<scripting>"],
+    correctIndex: 0,
+  },
+  {
+    text: "Where is the correct place to insert a JavaScript?",
+    choices: ["<head>", "<body>", "<footer>", "<scripting>"],
+    correctIndex: 1,
+  },
 ];
 
 // Variables for current question index and remaining time
@@ -42,88 +47,101 @@ goBackButton.addEventListener("click", goBack);
 // Function to start the game
 
 function startQuiz() {
-    splashScreenContainer.classList.add("hide");
-    quizContainer.classList.remove("hide");
-    timerElement.classList.remove("hide");
-// start timer and show first question
-    timer = setInterval(updateTime, 1000);
-    showQuestion();
+  startButton.classList.add("hide");
+  quizContainer.classList.remove("hide");
+  timerElement.classList.remove("hide");
+  // start timer and show first question
+  timer = setInterval(updateTime, 1000);
+  showQuestion();
 }
-
 
 // update the time remaining
 function updateTime() {
-    timeRemaining--;
-    timeRemainingElement.textcontent = timeRemaining;
-// to end quiz when timer hits 0
-if(timeRemaining <= 0) {
+  timeRemaining--;
+  timeRemainingElement.textContent = timeRemaining;
+  // to end quiz when timer hits 0
+  if (timeRemaining <= 0) {
     endQuiz();
-}
+  }
 }
 
 // Function to display the question and choices
 function showQuestion() {
-    const question = questions[currentQuestionIndex];
-    questionElement.innerText = question.text;
-    choicesContainer.innerHTML = "";
+  const question = questions[currentQuestionIndex];
+  questionElement.innerText = question.text;
+  choicesContainer.innerHTML = "";
 
-    questions.choices.forEach((choice, index) => {
-        const button = document.createElement("button");
-        button.innerText = choice;
-        button.addEventListener("click", () => handleAnswer(index));
-            choicesContainer.appendChild(button);
-        });
-    
+  question.choices.forEach((choice, index) => {
+    const button = document.createElement("button");
+    button.innerText = choice;
+    button.addEventListener("click", () => handleAnswer(index));
+    choicesContainer.appendChild(button);
+  });
 }
 // function that will handle the users choice
 function handleAnswer(choiceIndex) {
-    if (choiceIndex !== questions[currentQuestionIndex].correctIndex) {
-        timeRemaining = Math.max(0, timeRemaining - 10);
-        timeRemainingElement.textContent = timeRemaining;
-    }
+  if (choiceIndex !== questions[currentQuestionIndex].correctIndex) {
+    timeRemaining = Math.max(0, timeRemaining - 10);
+    timeRemainingElement.textContent = timeRemaining;
+  }
 
-    //Move to next question or the quiz will end
-    currentQuestionIndex++;
-    if (currentQuestionIndex < questions.length && timeRemaining > 0) {
-        showQuestion();
-    } else {
-        endQuiz();
-    }
+  //Move to next question or the quiz will end
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length && timeRemaining > 0) {
+    showQuestion();
+  } else {
+    endQuiz();
+  }
 }
 
 // Function that will end the quiz
-function endQuiz () {
-    clearInterval(timer);
-    quizContainer.classList.add("hide");
-    gamerOver.classList.remove("hide");
-    scoreElement.innerText = timeRemaining;
+function endQuiz() {
+  clearInterval(timer);
+  quizContainer.classList.add("hide");
+  gameOver.classList.remove("hide");
+  scoreElement.innerText = timeRemaining;
 }
 
 //function to save score
 
 function saveScore(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const initials = initialsInput.ariaValueMax.trim();
-    if (!initials) {
-        return;
-    }
+  const initials = initialsInput.value.trim();
+  if (!initials) {
+    return;
+  }
 
-    const highScores = JSON.parse(localStorage.getItem("highscores")) || [];
-    const newScore = { initials, score: timeRemaining };
-    highScores.push(newScore);
-    localStorage.setItem("highscores", JSON.stringify(highScores));
+  const highScores = JSON.parse(localStorage.getItem("highscores")) || [];
+  const newScore = { initials, score: timeRemaining };
+  highScores.push(newScore);
+  localStorage.setItem("highscores", JSON.stringify(highScores));
 
-    // Display initials and score next to the score element
-    const scoreDisplay = document.createElement('div');
-    scoreDisplay.innerHTML = `${initials}: ${timeRemaining}`;
-    scoreElement.parentNode.insertBefore(scoreDisplay, scoreElement.nextElementSibling);
+  // Display initials and score next to the score element
+  const scoreDisplay = document.createElement("div");
+  scoreDisplay.innerHTML = `${initials}: ${timeRemaining}`;
+  scoreElement.parentNode.insertBefore(scoreDisplay, scoreElement.nextElementSibling
+  );
 
-    // Hide initials input and save score button
-    initialsInput.classList.add("hide");
-    saveScoreButton.classList.add("hide");
+  // Hide initials input and save score button
+  initialsInput.classList.add("hide");
+  saveScoreButton.classList.add("hide");
+  
 }
 
+function goBack() {
+  const scoreDisplay = scoreElement.nextSibling;
+  if (scoreDisplay) {
+    scoreElement.parentNode.removeChild(scoreDisplay);
+  }
 
-
-
+  highScoresContainer.classList.add("hide");
+  gameOver.classList.add("hide");
+  initialsInput.classList.remove("hide");
+  saveScoreButton.classList.remove("hide");
+  startButton.classList.remove("hide");
+  timerElement.classList.add("hide");
+  currentQuestionIndex = 0;
+  timeRemaining = 60;
+  timeRemainingElement.textContent = timeRemaining;
+}
